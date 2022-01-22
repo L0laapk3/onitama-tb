8fb048f4105f2a501e3eb2db97400508acf4e447
L0laapk3 <L0laapk3@gmail.com>
2022-01-22 13:58:47 +0100

    more verbose with packing instructions

--- src/TableBase.h
+++ src/TableBase.h
@@ -9,13 +9,16 @@
 
 // the tablebase describes boards with player 0 to move.
 // if player 1 is to move, use boardToIndex<true> which flips the board.
-struct __attribute__((__packed__)) TableBase {
+struct TableBase {
 	typedef std::array<std::atomic<U64>*, PIECECOUNTMULT * KINGSMULT> Row;
+	
+	#pragma pack(push, 1)
 	std::array<Row, CARDSMULT> tb;
 	std::atomic<U64>* end;
+	#pragma pack(pop)
 
 	std::vector<std::atomic<U64>> mem;
-	
+
 	U64 cnt_0 = 0;
 	
     Row& operator [](int i) {
